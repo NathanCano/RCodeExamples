b74d02c0111a01e9b1e1b2e4f940f5b1129ffae9
NathanCano <51345449+NathanCano@users.noreply.github.com>
2019-06-08 02:49:13 -0700

    Update Ztest.R

--- Ztest.R
+++ Ztest.R
@@ -1,6 +1,4 @@
 #install.packages("TeachingDemos")
 library(TeachingDemos)
-
 x<-c(1001,1003,998,992,991,994,1002,999,986,996,997,988,1005,995)
 z.test(x, alternative= "two.sided", mu= 1000, stdev = 5.6457, conf.level = .95)
-
